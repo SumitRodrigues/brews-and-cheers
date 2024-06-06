728e67a8a44d80ad5a2e0ca2576524c98a4bb237
SumitRodrigues <sumitrod11@gmail.com>
2024-06-06 08:36:33 -0700

    Updated more content on homepage and gave nice background

--- app/layout.js
+++ app/layout.js
@@ -14,22 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="min-h-[87vh]">
-          <div class="relative h-full w-full bg-black">
-            <div class="absolute bottom-0 left-0 right-0 top-0 
-        bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)]
-         bg-[size:14px_24px]">
-            </div>
-            <div class="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full 
-         bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]">
-            </div>
-          </div>
-
-          {children}
+        <Navbar/>
+        <div className="min-h-[87vh] bg-[#000000] bg-[radial-gradient (#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        {children}
         </div>
-        <Footer />
-      </body>
+        <Footer/>
+        </body>
     </html>
   );
 }
