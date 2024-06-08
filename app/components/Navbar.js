139297cbbe74ff9a995e1e16d1327d682d87d458
SumitRodrigues <sumitrod11@gmail.com>
2024-06-08 12:05:16 -0700

    added more icons

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg-blue-950 text-white flex justify-between px-4 items-center h-16'>
-            <div className='logo font-bold text-lg'>Brews&Cheers</div>
+            <div className='logo font-bold text-lg flex items-center justify-center'>
+            <img src='beverage.gif' alt='' width={44}/>
+            <span>Brews & Cheers</span>
+            </div>
         <ul className='flex justify-between gap-4'>
             <li>Home</li>
             <li>About</li>
--- app/page.js
+++ app/page.js
@@ -3,33 +3,55 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex justify-center flex-col items-center gap-4 text-white h-[44vh]">
-      <div className="font-bold text-5xl flex gap-2 justify-center items-center">
-        Buy me a Beverage <span><img src="/beverage.gif" width={88} alt=""></img></span>
-      </div>
-      <p>
-        A crowdfunding platform for creators. Get funded by your fans and followers. Start now!
-      </p>
-      <div>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
+      <div className="flex justify-center flex-col items-center gap-4 text-white h-[44vh]">
+        <div className="font-bold text-5xl flex gap-2 justify-center items-center">
+          Buy me a Beverage <span><img src="/beverage.gif" width={88} alt=""></img></span>
+        </div>
+        <p>
+          A crowdfunding platform for creators. Get funded by your fans and followers. Start now!
+        </p>
+        <div>
+          <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
        focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
        text-sm px-5 py-2.5 text-center me-2 mb-2">Start Now</button>
-      <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
+          <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
        focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
        text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
-      </div>
-    </div>
-    <div className="bg-white h-1 opacity-10">
-      abc
-    </div>
-    <div>
-      <h1>Your Fans can buy you a Beverage</h1>
-      <div className="flex gap-5">
-        <div className="item">
-          <img src="/man.gif" alt=""></img>
         </div>
       </div>
-    </div>
+      <div className="bg-white h-1 opacity-10">
+        abc
+      </div>
+      <div className="text-white container mx-auto pb-32 pt-14">
+        <h1 className="text-3xl font-bold text-center mb-14">Your Fans can buy you a Beverage</h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt=""></img>
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt=""></img>
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Fans are available for you to help you</p>
+          </div>
+          <div className="item space-y-3 flex flex-col items-center justify-center">
+            <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt=""></img>
+            <p className="font-bold">Fans want to help</p>
+            <p className="text-center">Fans are available for you to help you</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white h-1 opacity-10">
+        abc
+      </div>
+      <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-center mb-14">Learn more about us</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Ey-mwAKztWk?si=hvOU7_MCTfzW1xwi"
+          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+    gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
+      </div>
     </>
   );
 }
