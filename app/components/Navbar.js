cb8da988a5a988f9a619a7d0c8ebc30db811ebc3
SumitRodrigues <sumitrod11@gmail.com>
2024-06-11 12:00:21 -0700

    Added two more buttons in navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -25,10 +25,18 @@ const Navbar = () => {
             <li>Login</li>
         </ul> */}
       <div>
-        <Link href={"/login"}>
-        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
-       focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
-       text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>
+        {session && <Link href={"/dashboard"}><button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
+        focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
+        text-sm px-5 py-2.5 text-center me-2 mb-2" >Dashboard</button></Link>}
+        
+        {session && <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
+        focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
+        text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => { signOut() }}>Logout</button>}
+
+        {!session && <Link href={"/login"}>
+        <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
+        focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
+        text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>}
       </div>
     </nav>
   )
